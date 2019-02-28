a3deb661d8a0850ad4e9ac7709e1dac3f8195e16
Sebastian Sebald <sebastian.sebald@gmail.com>
2019-02-28 12:02:00 +0100

    ✨ Jest Preset (#15)
    - Introduce jest preset
    - Add travis config (not yet used, since the repo is still private)

---
+++ packages/jest-preset/README.md
@@ -0,0 +1,11 @@
+# `jest-preset-unscripted`
+
+> TODO: description
+
+## Usage
+
+```
+const caboodleUtils = require('jest-preset-caboodle');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/jest-preset/jest-preset.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // Setup
+  // ---------------
+  preset: 'ts-jest',
+
+  // Paths
+  // ---------------
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.ts?(x)'],
+
+  // Coverage
+  // ---------------
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+
+    /**
+     * Ignore
+     * - type definition files
+     * - index files (they should only be used as barrels, see https://github.com/basarat/typescript-book/blob/master/docs/tips/barrel.md)
+     */
+    '!**/{*.d.ts,index.ts}',
+
+    // Ignore node_modules and build folders
+    '!**/node_modules/**',
+    '!**/build/**',
+  ],
+
+  globals: {
+    // Ts-jest configuration
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true,
+      },
+    },
+  },
+};
