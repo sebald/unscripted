e4427d77a478f7ad1b5cce53425f2f040ad796c9
Sebastian Sebald <sebastian.sebald@reservix.de>
2019-02-28 14:04:24 +0100

    🌈 Remove debug lines

--- packages/utils/src/utils.ts
+++ packages/utils/src/utils.ts
@@ -4,7 +4,6 @@ import find from 'find-up';
 export const getProjetRoot = async (
   cwd = process.cwd()
 ): Promise<string | null> => {
-  console.log(cwd);
   const pkg = await find('package.json', { cwd });
   return pkg ? dirname(pkg) : null;
 };
