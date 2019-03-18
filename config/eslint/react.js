87db25150ea1b15fb6a508b86aa5bc62afc72304
Sebastian Sebald <sebastian.sebald@reservix.de>
2019-03-18 16:46:55 +0100

    🌈 Improve eslint config for react

--- config/eslint/react.js
+++ config/eslint/react.js
@@ -2,6 +2,10 @@ const createConfig = require('./create');
 
 module.exports = createConfig(['xo-react/space'], {
   /**
+   * Propt type validation is done via TypeScript.
+   */
+  'react/prop-types': 'off',
+  /**
    * Enforcing return types is annoying with React and TS,
    * since usually you're using `React.FC` anyway.
    */
